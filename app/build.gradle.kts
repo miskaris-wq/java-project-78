0776793b7c5f2da1eeb00ae490798f46e3df7216
Ксения Рассказова <krasskazova@test.ru>
2025-04-12 00:19:25 +0300

    fix 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,9 +8,6 @@ plugins {
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
-application {
-    mainClass = "hexlet.code.App"
-}
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
--- app/src/main/java/hexlet/code/App.java
+++
@@ -1,7 +0,0 @@
-package hexlet.code;
-
-public class App {
-    public static void main(String[] args) {
-        // не знаю что тут должно быть :с
-    }
-}
