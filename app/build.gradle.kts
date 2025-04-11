94f6578b645e99b894d1a2e7b2fe4195017d4039
Ксения Рассказова <krasskazova@test.ru>
2025-04-11 21:57:58 +0300

    fix 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     application
     checkstyle
@@ -23,9 +26,18 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 
 tasks.named("build") {
     dependsOn("checkstyleMain")
     dependsOn("checkstyleTest")
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
